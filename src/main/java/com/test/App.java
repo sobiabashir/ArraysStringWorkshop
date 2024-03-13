bde10313b58540afc0b09bc0a08c080974cbffcf
Sobia Bashir <sobiabashir_786@hotmail.com>
2024-03-13 21:48:50 +0100

    new class Nameformatter is used for task 1

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,4 +1,5 @@
 package com.test;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
@@ -9,20 +10,43 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the number of names you want to store: ");
+        int numOfNames = scanner.nextInt();
+        scanner.nextLine();
+
+        NameFormatter nameFormatter = new NameFormatter();
+        String[] formatedOfName = new String[numOfNames];
         // Reading names from the user
-        System.out.print("Enter names separated by commas: ");
+        //System.out.print("Enter names separated by commas: ");
         String namesListOFUser = scanner.nextLine();
 
-        // Splitting the input string into an array of names
-        String[] initialNames = namesListOFUser.split(",");
 
-        // Trim each name to remove leading and trailing whitespaces
-        for (int i = 0; i < initialNames.length; i++)
+        for (int i = 0; i < numOfNames; i++)
         {
-            initialNames[i] = initialNames[i].trim();
+            System.out.print("Enter name " + (i + 1) + ": ");
+            String fullName = scanner.nextLine();
+            formatedOfName[i] = Arrays.toString(nameFormatter.formatName(fullName));
+            formatedOfName[i]=formatedOfName[i].trim();
         }
 
-        NameRepository.setNames(initialNames);
+        System.out.println("\nStored formatted names:");
+        for (String name : formatedOfName) {
+            System.out.println(name);
+        }
+
+
+        // Splitting the input string into an array of names
+       // String[] initialNames = namesListOFUser.split(",");
+
+        // Trim each name to remove leading and trailing whitespaces
+       // for (int i = 0; i < initialNames.length; i++)
+        //{
+          //  initialNames[i] = initialNames[i].trim();
+        //}
+
+
+
+        NameRepository.setNames(formatedOfName);
 
         System.out.println("Number of names: " + NameRepository.getSize());
 
@@ -31,6 +55,8 @@ public class App
         // Getting name to find from the user
         System.out.print("Enter a name to find: ");
         String nameToFind = scanner.nextLine();
+        /////// conver the user input according to the defined formate [firstname, ,lastname]
+        nameToFind=Arrays.toString(nameFormatter.formatName(nameToFind));
         String foundName = NameRepository.find(nameToFind);
         if (foundName != null) {
             System.out.println("Name found: " + foundName);
@@ -41,6 +67,8 @@ public class App
         // Getting name to add from the user
         System.out.print("Enter a name to add: ");
         String nameToAdd = scanner.nextLine();
+        nameToAdd=Arrays.toString(nameFormatter.formatName(nameToAdd));
+
         boolean added = NameRepository.add(nameToAdd);
         if (added) {
             System.out.println("Name added successfully: " + nameToAdd);
@@ -48,7 +76,6 @@ public class App
             displyNames();
         } else {
             System.out.println("Name already exists: " + nameToAdd);
-            System.out.println("After removing list is below:");
             displyNames();
         }
 
