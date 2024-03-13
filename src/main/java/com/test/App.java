bb980d466602a54ea44665c886616eaaad552d36
Sobia Bashir <sobiabashir_786@hotmail.com>
2024-03-13 12:54:34 +0100

    project started

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -1,5 +1,6 @@
 package com.test;
-
+import java.util.Arrays;
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -8,7 +9,13 @@ public class App
 {
     public static void main( String[] args )
     {
+        NameStyle stringObj =new NameStyle();
 
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the full name in the following pattern first_name  last_name : ");
+        String fullName = scanner.nextLine();
+        String formattedName = Arrays.toString(stringObj.formatName(fullName));
+        System.out.println("Formatted name: " + formattedName);
+
     }
 }
